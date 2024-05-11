b7cbe6c31ac449510c57bb2b3ae1c0a47ddb1f06
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-11 17:58:05 +0200

    J'ai fini le menu principal dis moi si c bien ou s'il manque des trucs :)

--- main.c
+++ main.c
@@ -45,11 +45,67 @@ int main() {
         printf("13. Afficher le nom d'une colonne\n");
         printf("14. Afficher le nombre de lignes\n");
         printf("15. Afficher le nombre de colonnes\n");
-        printf("16. Afficher le nombre de cellules contenant une valeur supérieure à une valeur donnée\n");
-        printf("17. Afficher le nombre de cellules contenant une valeur inférieure à une valeur donnée\n");
-        printf("18. Afficher le nombre de cellules contenant une valeur égale à une valeur donnée\n")
+        printf("16. Afficher le nombre de cellules contenant une valeur égale à une valeur donnée\n");
+        printf("17. Afficher le nombre de cellules contenant une valeur supérieure à une valeur donnée\n");
+        printf("18. Afficher le nombre de cellules contenant une valeur inférieure à une valeur donnée\n");
         printf("19. Quitter le menu principal\n\n");
         printf("Choix : ");
         scanf("%d", &choix);
-    }
-    
+        if(choix==1){
+            int insertion_valeur(FRAME* frame, int value);
+        }
+        if(choix==2){
+            void affichage_dataframe(FRAME* frame);
+        }
+        if(choix==3){
+            void affichage_ligne(FRAME* frame);
+        }
+        if(choix==4){
+            void affichage_colonne(FRAME* frame);
+        }
+        if(choix==5){
+            void ajouter_ligne(FRAME* frame);
+        }
+        if(choix==6){
+            void ajouter_colonne(FRAME* frame);
+        }
+        if(choix==7){
+            void supprimer_ligne(FRAME** frame);
+        }
+        if(choix==8){
+            void supprimer_colonne(FRAME** frame);
+        }
+        if(choix==9){
+            void renommer_colonne(FRAME* frame);
+        }
+        if(choix==10){
+            void recherche_valeur(FRAME* frame, int valeur);
+        }
+        if(choix==11){
+            void affichage_case(FRAME* frame);
+        }
+        if(choix==12){
+            void remplacer_case(FRAME* frame);
+        }
+        if(choix==13){
+            void affichage_nom_colonne(FRAME* frame);
+        }
+        if(choix==14){
+            void affichage_nb_ligne(FRAME* frame);
+        }
+        if(choix==15){
+            void affichage_nb_colonne(FRAME* frame);
+        }
+        if(choix==16){
+            int nb_valeur_egal_frame(FRAME* frame, int valeur);
+        }
+        if(choix==17){
+            int nb_valeur_superieur_frame(FRAME* frame, int valeur);
+        }
+        if(choix==18){
+            int nb_valeur_inferieur_frame(FRAME* frame, int valeur);
+        }
+        if(choix==19){
+            printf("Merci d'avoir utilisé notre service\nAu plaisir de vous revoir !\n");
+            return 0;
+        }
