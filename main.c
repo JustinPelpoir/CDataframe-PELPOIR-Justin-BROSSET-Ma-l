01b1f413ba322e368cb5517d6afed00a26088573
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-11 16:06:30 +0200

    j'ai oublié un guillemet

--- main.c
+++ main.c
@@ -30,9 +30,9 @@ int main() {
         printf("\n----------Menu Principal----------\n");
         printf("Bienvenue dans le menu principal,\nQuelle est votre requête ?\n\n")
         printf("1. Créer une colonne\n");
-        printf("2. Insérer une valeur\n);
+        printf("2. Insérer une valeur\n");
         printf("3. Supprimer une colonne\n");
-        printf("4. Afficher une colonne\n");
+        printf("4. Afficher une colonne\n"); 
         printf("5. Afficher le nombre de d’occurrences de la valeur donnée\n");
         printf("6. Afficher la valeur présente à une position donnée\n");
         printf("7. Afficher le nombre de valeurs qui sont supérieures à la valeur donnée\n");
