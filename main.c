68c685a4dfbb31c3e4ed102fb841bcdb4119cc0d
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-11 17:14:39 +0200

    j'ai modifié car le menu appelait pas les bonnes fonctions

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 
 #include "column.h"
 #include <stdio.h>
+#include "cdataframe.h"
 
 
 /** 
@@ -28,17 +29,26 @@ int main() {
     do while {
         // Affichage du menu
         printf("\n----------Menu Principal----------\n");
-        printf("Bienvenue dans le menu principal,\nQuelle est votre requête ?\n\n")
-        printf("1. Créer une colonne\n");
-        printf("2. Insérer une valeur\n");
-        printf("3. Supprimer une colonne\n");
-        printf("4. Afficher une colonne\n"); 
-        printf("5. Afficher le nombre de d’occurrences de la valeur donnée\n");
-        printf("6. Afficher la valeur présente à une position donnée\n");
-        printf("7. Afficher le nombre de valeurs qui sont supérieures à la valeur donnée\n");
-        printf("8. Afficher le nombre de valeurs qui sont inférieures à la valeur donnée\n");
-        printf("9. Afficher le nombre de valeurs qui sont égales à la valeur donnée\n");
-        printf("10. Quitter le menu principal\n\n");
+        printf("Bienvenue dans le menu principal,\nQuelle est votre requête ?\n\n");
+        printf("1. Insérer une valeur\n");
+        printf("2. Afficher le dataframe\n");
+        printf("3. Afficher une partie des lignes\n");
+        printf("4. Afficher un partie des colonnes\n");
+        printf("5. Ajouter une ligne\n");
+        printf("6. Ajouter une colonne\n");
+        printf("7. Supprimer une ligne\n");
+        printf("8. Supprimer une colonne\n"); 
+        printf("9. Renommer une colonne\n");
+        printf("10. Rechercher une valeur\n");
+        printf("11. Afficher une case\n");
+        printf("12. Remplacer une case\n");
+        printf("13. Afficher le nom d'une colonne\n");
+        printf("14. Afficher le nombre de lignes\n");
+        printf("15. Afficher le nombre de colonnes\n");
+        printf("16. Afficher le nombre de cellules contenant une valeur supérieure à une valeur donnée\n");
+        printf("17. Afficher le nombre de cellules contenant une valeur inférieure à une valeur donnée\n");
+        printf("18. Afficher le nombre de cellules contenant une valeur égale à une valeur donnée\n")
+        printf("19. Quitter le menu principal\n\n");
         printf("Choix : ");
         scanf("%d", &choix);
     }
