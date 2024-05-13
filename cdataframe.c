6489bca0f9871a3d9d1367018622085fb55f8cc3
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-13 08:36:37 +0200

    Update cdataframe.c

--- cdataframe.c
+++ cdataframe.c
@@ -2,3 +2,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define REALOC_SIZE 256
+
+// Création d'un dataframe
+FRAME *create_dataframe(char* title)
+{
+    FRAME *c = (COLUMN*)malloc(sizeof(COLUMN));
+    c-> titre = title;
+    c-> TL = 0;
+    c-> TP = 0;
+    c-> T = NULL;
+
+    return c;
+}
