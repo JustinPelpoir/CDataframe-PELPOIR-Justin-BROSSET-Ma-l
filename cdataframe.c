2c5088c3777d3289d67225a7e62ff9873296cca2
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-13 08:38:00 +0200

    Update cdataframe.c

--- cdataframe.c
+++ cdataframe.c
@@ -6,7 +6,7 @@
 // Création d'un dataframe
 FRAME *create_dataframe(char* title)
 {
-    FRAME *c = (COLUMN*)malloc(sizeof(COLUMN));
+    FRAME *c = (FRAME*)malloc(sizeof(FRAME));
     c-> titre = title;
     c-> TL = 0;
     c-> TP = 0;
