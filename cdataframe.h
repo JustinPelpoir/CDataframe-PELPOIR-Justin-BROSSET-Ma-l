f4810f885db658575131742eed24554ec1aedff6
qqn-que-tu-connais-pt <150938124+qqn-que-tu-connais-pt@users.noreply.github.com>
2024-05-19 17:04:24 +0200

    Update cdataframe.h

--- cdataframe.h
+++ cdataframe.h
@@ -25,9 +25,9 @@ int insertion_valeur(FRAME* frame, int value);
 
 
 // Affichages :
-void affichage_dataframe(FRAME* frame);
-void affichage_ligne(FRAME* frame);
-void affichage_colonne(FRAME* frame);
+void affichage_dataframe(FRAME* frame); // A faire
+void affichage_ligne(FRAME* frame);// A faire
+void affichage_colonne(FRAME* frame);// A faire
 
 
 
@@ -35,20 +35,20 @@ void affichage_colonne(FRAME* frame);
 void ajouter_ligne(FRAME* frame);
 void ajouter_colonne(FRAME* frame);
 void supprimer_ligne(FRAME** frame);
-void supprimer_colonne(FRAME** frame);
+void supprimer_colonne(FRAME** frame);// A faire
 
-void renommer_colonne(FRAME* frame);
-void recherche_valeur(FRAME* frame, int valeur);
+void renommer_colonne(FRAME* frame);// A faire
+void recherche_valeur(FRAME* frame, int valeur);// A faire
 
-void affichage_case(FRAME* frame);
-void remplacer_case(FRAME* frame);
+void affichage_case(FRAME* frame);// A faire
+void remplacer_case(FRAME* frame);// A faire
 
-void affichage_nom_colonne(FRAME* frame);
+void affichage_nom_colonne(FRAME* frame);// A faire
 void affichage_nb_ligne(FRAME* frame);
-void affichage_nb_colonne(FRAME* frame);
+void affichage_nb_colonne(FRAME* frame);// A faire
 
-int nb_valeur_egal_frame(FRAME* frame, int valeur);
-int nb_valeur_superieur_frame(FRAME* frame, int valeur);
+int nb_valeur_egal_frame(FRAME* frame, int valeur);// A faire
+int nb_valeur_superieur_frame(FRAME* frame, int valeur);// A faire
 int nb_valeur_inferieur_frame(FRAME* frame, int valeur);
 
 
